47c0e5426041a0a92f865a27e280540f7b90687b
Sultan Kautsar <42274355+bydzen@users.noreply.github.com>
2022-03-15 13:49:43 +0700

    Add grid list project layout 🎉

---
+++ gridlist_proj/main.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const GridListSK());
+}
+
+class GridListSK extends StatelessWidget {
+  const GridListSK({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Grid List SK',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Grid List by 1303194010'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: GridView(
+        padding: const EdgeInsets.all(10.0),
+        children: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const [
+              Text(
+                "Latihan Layout",
+                style: TextStyle(
+                    color: Colors.black, fontSize: 16, fontFamily: "Fira Code"),
+              ),
+            ],
+          ),
+          Container(
+            color: Colors.brown,
+            child: const Text(
+              "Item 1",
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+          Container(
+            color: Colors.blue,
+            child: const Text(
+              "Item 1",
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+          Container(
+            color: Colors.green,
+            child: const Text(
+              "Item 1",
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+          Container(
+            color: Colors.yellow[700],
+            child: const Text(
+              "Item 1",
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+          Container(
+            color: Colors.red,
+            child: const Text(
+              "Item 1",
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+        ],
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          mainAxisSpacing: 10,
+          crossAxisSpacing: 10,
+        ),
+      ),
+    );
+  }
+}
