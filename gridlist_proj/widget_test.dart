4c9454d1961b6ba80c8afb431f56da781b691db0
Sultan Kautsar <42274355+bydzen@users.noreply.github.com>
2022-03-16 16:59:41 +0700

    Update v2 widget_test.dart ✔

--- gridlist_proj/widget_test.dart
+++ gridlist_proj/widget_test.dart
@@ -2,7 +2,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:grid_layout/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('Grid Layout Test', (WidgetTester tester) async {
     await tester.pumpWidget(const GridListSK());
   });
 }
